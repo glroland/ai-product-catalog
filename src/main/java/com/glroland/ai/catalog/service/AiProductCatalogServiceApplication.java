dfaeb050475920bc06e0b696210304c3143c07ce
Lee Roland <glroland@hotmail.com>
2024-06-27 14:06:13 -0400

    initial code generation

---
+++ src/main/java/com/glroland/ai/catalog/service/AiProductCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.glroland.ai.catalog.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProductCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProductCatalogServiceApplication.class, args);
+	}
+
+}
