be74f091ade08e897095eb0cf0343dce5237ba60
Lee Roland <glroland@hotmail.com>
2024-06-28 11:14:47 -0400

    search api now being invoked.  returning mock values rather than performing database search though

--- src/main/java/com/glroland/ai/catalog/service/ProductServiceController.java
+++ src/main/java/com/glroland/ai/catalog/service/ProductServiceController.java
@@ -1,8 +1,15 @@
 package com.glroland.ai.catalog.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.glroland.ai.catalog.service.dao.ProductDAO;
@@ -11,18 +18,36 @@ import com.glroland.ai.catalog.service.entities.Product;
 @RestController
 public class ProductServiceController 
 {
+    private static final Log log = LogFactory.getLog(ProductServiceController.class);
+    
     @Autowired
     private ProductDAO productDAO;
 
     @GetMapping("/greeting")
     public String greeting()
     {
-        return "Hello";
+        return "Hello!";
     }
 
     @GetMapping("/product/{id}")
     public Product getProduct(@PathVariable Integer id)
     {
         return productDAO.getProduct(id);
+    }
+
+    @PostMapping("/search")
+    public List<Product> search(@RequestParam(value="category", required=false) String category,
+                                @RequestParam(value="brand", required=false) String brand,
+                                @RequestParam(value="sku", required=false) String sku,
+                                @RequestParam(value="size", required=false) String size,
+                                @RequestParam(value="nameDesc", required=false) String nameDesc
+     )
+    {
+        log.info("Search API called with the following parameters: category=" + category + " brand=" + brand + " sku=" + sku + " size=" + size + " nameDesc=" + nameDesc);
+        
+        ArrayList<Product> list = new ArrayList<Product>();
+        for (int i=0; i<10; i++)
+            list.add(productDAO.getProduct(1));
+        return list;
     }
 }
