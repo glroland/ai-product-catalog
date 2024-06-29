7cf2a9261696d32711c2e194c3752f4fb2413e08
Lee Roland <glroland@hotmail.com>
2024-06-28 21:11:35 -0400

    Removed an unused import

--- src/main/java/com/glroland/ai/catalog/agent/ChatServiceController.java
+++ src/main/java/com/glroland/ai/catalog/agent/ChatServiceController.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
