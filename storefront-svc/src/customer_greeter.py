bb38f036c88d8687b2fcb03cdabaf9f2ad40bc14
Lee Roland <glroland@hotmail.com>
2024-08-18 20:18:33 -0400

    cleaned up linting

--- storefront-svc/src/customer_greeter.py
+++ storefront-svc/src/customer_greeter.py
@@ -2,16 +2,13 @@
 
 Analyzes a new customer inquiry to determine if its a qualified opportunity or not.
 """
-import httpx
 import logging
-from langchain_openai import ChatOpenAI
-from openai import APIConnectionError
 from langchain_core.messages import HumanMessage, SystemMessage
 from openai_client import openai_invoke
 
 logger = logging.getLogger(__name__)
 
-def qualify_customer_action(userMessage):
+def qualify_customer_action(user_message):
     """ Qualify whether a new customer walking into the virtual store is interested in
         shoes.
 
@@ -19,28 +16,28 @@ def qualify_customer_action(userMessage):
     """
     logger.info("qualify_customer_action()")
 
-    logger.info("User Message = " + userMessage)
+    logger.info("User Message = " + user_message)
 
     messages = [
         SystemMessage(content="You are a retail store manager for a Nike shoe store. " +
                               "You are friendly and only give concise answers to questions. " +
                               "Do not answer questions unrelated to selling shoes. " +
                               "Politely turn away any customers not interested in shoes." +
-                              "Simply respond with only ACCEPT if the customer's questions should " +
-                              "be answered and only REJECT if the customer should be turned away."),
-        HumanMessage(content=userMessage),
+                              "Simply respond with only ACCEPT if the customer's questions " +
+                              "should be answered and only REJECT if the customer should " +
+                              "be turned away."),
+        HumanMessage(content=user_message),
     ]
 
     response = openai_invoke(messages, max_tokens=10, temperature=0)
-    responseMessage = response.content
+    response_message = response.content
 
-    logger.info("Response: " + responseMessage)
-    if "ACCEPT" in responseMessage.upper():
+    logger.info("Response: " + response_message)
+    if "ACCEPT" in response_message.upper():
         return True
-    elif "REJECT" in responseMessage.upper():
+    if "REJECT" in response_message.upper():
         return False
 
-    msg = "Unexpected response from LLM: " + responseMessage
+    msg = "Unexpected response from LLM: " + response_message
     logger.fatal(msg)
     raise msg
-
