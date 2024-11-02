d3612465fd238c4f7d6bddbae14e71d4c62eff7c
Lee Roland <glroland@hotmail.com>
2024-11-02 13:29:42 -0400

    trying to get db user correct

--- data/sql_ddl/create_db.sql
+++ data/sql_ddl/create_db.sql
@@ -1,5 +1,4 @@
 create user ai_product_catalog with password 'ai_product_catalog123';
 ALTER USER ai_product_catalog WITH SUPERUSER;
 
-create database ai_product_catalog with owner ai_product_catalog;
-
+create database 'ai-product-catalog' with owner ai_product_catalog;
