1d87e74981a7716bc75418e8dbfe0ba31e8cdf2d
Lee Roland <glroland@hotmail.com>
2025-01-07 19:34:09 +0000

    Updated pipeline to create schema.  failing because of pgvector

--- data/python_ddl/create_db.py
+++ data/python_ddl/create_db.py
@@ -12,22 +12,34 @@ def main():
     if "DB_PORT" in os.environ:
         db_host = os.environ["DB_PORT"]
 
-    db_user = "postgres"
+    db_dba_user = "postgres"
     if "DB_DBA_USER" in os.environ:
-        db_user = os.environ["DB_DBA_USER"]
+        db_dba_user = os.environ["DB_DBA_USER"]
 
-    db_password = "r3dh@t123"
+    db_dba_password = "r3dh@t123"
     if "DB_DBA_PASSWORD" in os.environ:
-        db_password = os.environ["DB_DBA_PASSWORD"]
+        db_dba_password = os.environ["DB_DBA_PASSWORD"]
 
-    db_conn_str = f"host={db_host} port={db_port} user={db_user} password={db_password}"
+    db_name = "ai_product_catalog"
+    if "DB_NAME" in os.environ:
+        db_name = os.environ["DB_NAME"]
+
+    db_user = "ai-product-catalog"
+    if "DB_USER" in os.environ:
+        db_user = os.environ["DB_USER"]
+
+    db_password = "ai_product_catalog123"
+    if "DB_PASSWORD" in os.environ:
+        db_password = os.environ["DB_PASSWORD"]
+
+    db_conn_str = f"host={db_host} port={db_port} user={db_dba_user} password={db_dba_password}"
     print (db_conn_str)
 
     with psycopg.connect(db_conn_str, autocommit=True) as db_connection:
         with db_connection.cursor() as c:
-            c.execute("create user 'ai-product-catalog' with password 'ai_product_catalog123'")
-            c.execute("ALTER USER 'ai-product-catalog' WITH SUPERUSER")
-            c.execute("create database ai_product_catalog with owner ai_product_catalog")
+            c.execute(f"create user '{db_user}' with password '{db_password}'")
+            c.execute(f"ALTER USER '{db_user}' WITH SUPERUSER")
+            c.execute(f"create database {db_name} with owner {db_user}")
 
 
 if __name__ == "__main__":
--- data/python_ddl/drop_db.py
+++ data/python_ddl/drop_db.py
@@ -12,25 +12,33 @@ def main():
     if "DB_PORT" in os.environ:
         db_port = os.environ["DB_PORT"]
 
-    db_user = "postgres"
+    db_dba_user = "postgres"
     if "DB_DBA_USER" in os.environ:
-        db_user = os.environ["DB_DBA_USER"]
+        db_dba_user = os.environ["DB_DBA_USER"]
 
-    db_password = "r3dh@t123"
+    db_dba_password = "r3dh@t123"
     if "DB_DBA_PASSWORD" in os.environ:
-        db_password = os.environ["DB_DBA_PASSWORD"]
+        db_dba_password = os.environ["DB_DBA_PASSWORD"]
 
-    db_conn_str = f"host={db_host} port={db_port} user={db_user} password={db_password}"
+    db_name = "ai_product_catalog"
+    if "DB_NAME" in os.environ:
+        db_name = os.environ["DB_NAME"]
+
+    db_user = "ai-product-catalog"
+    if "DB_USER" in os.environ:
+        db_user = os.environ["DB_USER"]
+
+    db_conn_str = f"host={db_host} port={db_port} user={db_dba_user} password={db_dba_password}"
     print (db_conn_str)
 
     with psycopg.connect(db_conn_str, autocommit=True) as db_connection:
         with db_connection.cursor() as c:
             try:
-                c.execute("drop database ai_product_catalog")
+                c.execute(f"drop database {db_name}")
             except Exception as e:
                 print ("Ignoring psycopg.errors.InvalidCatalogName.", e)
             try:
-                c.execute("drop user ai_product_catalog")
+                c.execute(f"drop user {db_user}")
             except Exception as e:
                 print ("Ignoring psycopg.errors.InvalidCatalogName.", e)
 
